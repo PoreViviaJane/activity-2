e79a2b67d8c7442e9f6d3aab62dbfe759bd1a8b2
PoreViviaJane <vivianjane.pore@evsu.edu.ph>
2021-04-07 22:45:14 +0800

    commit files

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,51 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
 
+contName = "";
+  contNumber = "";
+  contacts: any = [];
+
+  constructor(public alertCtrl: AlertController) { }
+
+  saveCont() {
+    let contact = {
+      name: this.contName,
+      number: this.contNumber
+    }
+      this.contacts.push(contact);
+      this.clearField();
+    }
+
+  clearField() {
+    this.contName = "";
+    this.contNumber = "";
+  }
+
+  async deleteCont(cont){
+    const confirm = await this.alertCtrl.create({
+      message: 'Are you sure you want to delete this contact?',
+      buttons: [
+        {
+          text: 'Back',
+          role: 'cancel',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        },
+        {
+          text: 'Delete',
+          handler: () => {
+            let index = this.contacts.indexOf(cont);
+            if(index > -1){
+              this.contacts.splice(index, 1);
+            }
+          }
+        }
+      ]
+    });
+  await confirm.present();
+}
+
+ 
 }
