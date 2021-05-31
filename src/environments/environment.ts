5bc7ea18c8468d23eb563db77c60be45faf21c13
PoreViviaJane <vivianjane.pore@evsu.edu.ph>
2021-06-01 00:03:45 +0800

    activity 5

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+  apiKey: "AIzaSyAf4hdnp81I5Xm3lScPU_EuLD6fIY5Qvgg",
+  authDomain: "myapp-e7f08.firebaseapp.com",
+  databaseURL: "https://myapp-e7f08-default-rtdb.firebaseio.com",
+  projectId: "myapp-e7f08",
+  storageBucket: "myapp-e7f08.appspot.com",
+  messagingSenderId: "16830086396",
+  appId: "1:16830086396:web:1323e52b33ed24bc91ab79",
+  measurementId: "G-7259MV3FZT"
+}
 };
 
 /*
